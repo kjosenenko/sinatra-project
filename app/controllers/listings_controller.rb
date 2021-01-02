3082873b0f690fb7933cc87d07847ae56376771f
KJ Osenenko <karljosenenko@gmail.com>
2021-01-02 13:57:10 -0700

    added views and routing for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,3 +1,41 @@
 class ListingsController < ApplicationController
 
+    get '/listings' do
+        @listings = Listing.all
+        erb :'listings/index'
+    end
+
+    get '/listings/new' do
+        erb :'listings/new'
+    end
+
+    post '/listings' do
+        @listing = Listing.create(params)
+        redirect to "/listings/#{@listing.id}"
+    end
+
+    get '/listings/:id' do
+        @listing = Listing.find_by_id(params[:id])
+        erb :'listings/show'
+    end
+
+    get '/listings/:id/edit' do
+        @listing = Listing.find_by_id(params[:id])
+        erb :'listings/edit'
+    end
+
+    patch '/listings/:id' do
+        @listing = Listing.find_by_id(params[:id])
+        @listing.title = (params[:title])
+        @listing.description = (params[:description])
+        @listing.save
+        redirect to "/listings/#{@listing.id}"
+    end
+
+    delete '/listings/:id' do
+        @listing = Listing.find_by_id(params[:id])
+        @listing.delete
+        redirect to "listings"
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 User.destroy_all
+Listing.destroy_all
+Bid.destroy_all
 
-User.create(:username => "Leo", :password => "password")
-User.create(:username => "Ralph", :password => "password")
-User.create(:username => "Mikey", :password => "password")
-User.create(:username => "Donnie", :password => "password")
+leo = User.create(:username => "Leo", :password => "password")
+ralph = User.create(:username => "Ralph", :password => "password")
+mikey = User.create(:username => "Mikey", :password => "password")
+donnie = User.create(:username => "Donnie", :password => "password")
+
+Listing.create(:title => "job", :description => "A job", user:leo)
