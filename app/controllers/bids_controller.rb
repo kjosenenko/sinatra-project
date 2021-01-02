516d2762ee3f96ccef23757b286ba1af2bb5a02a
KJ Osenenko <karljosenenko@gmail.com>
2021-01-02 16:22:26 -0700

    added views and routing for bids

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,3 +1,57 @@
 class BidsController < ApplicationController
 
+    #index all bids made by user
+    get '/bids' do
+        @bids = Bid.all
+        erb :'bids/index'
+    end
+
+    #index all bids for a listing
+    get '/listings/:id/bids' do
+        @listing = Listing.find_by_id(params[:id])
+        @bids = Bid.select {|bid| bid.listing_id == params[:id].to_i}
+        erb :'bids/index'
+    end
+
+    #create new bid form
+    get '/bids/new/:id' do
+        @listing = Listing.find_by_id(params[:id])
+        erb :'bids/new'
+    end
+
+    #post new bid
+    post '/bids' do
+        @bid = Bid.create(params)
+        redirect to "/bids/#{@bid.id}"
+    end
+
+    #show a bid
+    get '/bids/:id' do
+        @bid = Bid.find_by_id(params[:id])
+        erb :'bids/show'
+    end
+
+    #edit a bid form
+    get '/bids/:id/edit' do
+        @bid = Bid.find_by_id(params[:id])
+        erb :'bids/edit'
+    end
+
+    #update bid in db
+    patch '/bids/:id' do
+        @bid = Bid.find_by_id(params[:id])
+        @bid.amount = (params[:amount])
+        @bid.message = (params[:message])
+        @bid.save
+        redirect to "/bids/#{@bid.id}"
+    end
+
+    #delete a bid
+    delete '/bids/:id' do
+        @bid = Bid.find_by_id(params[:id])
+        @bid.delete
+        redirect to "listings"
+        # will redirect to user's bids
+    end
+
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,29 +1,35 @@
 class ListingsController < ApplicationController
 
+    #index all listings
     get '/listings' do
         @listings = Listing.all
         erb :'listings/index'
     end
 
+    #create new listing form
     get '/listings/new' do
         erb :'listings/new'
     end
 
+    #post new listing
     post '/listings' do
         @listing = Listing.create(params)
         redirect to "/listings/#{@listing.id}"
     end
 
+    #show a listing
     get '/listings/:id' do
         @listing = Listing.find_by_id(params[:id])
         erb :'listings/show'
     end
 
+    #edit a listing form
     get '/listings/:id/edit' do
         @listing = Listing.find_by_id(params[:id])
         erb :'listings/edit'
     end
 
+    #update lisitng in db
     patch '/listings/:id' do
         @listing = Listing.find_by_id(params[:id])
         @listing.title = (params[:title])
@@ -32,6 +38,7 @@ class ListingsController < ApplicationController
         redirect to "/listings/#{@listing.id}"
     end
 
+    #delete a listing
     delete '/listings/:id' do
         @listing = Listing.find_by_id(params[:id])
         @listing.delete
